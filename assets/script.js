a3b3556d65311de795548509e9fcea96506519ec
L0nginu5 <gochomasa@gmail.com>
2024-10-27 04:33:10 +0900

    諸々修正

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,3 @@
-// 動画と要素の取得
-const video = document.getElementById('intro-video');
-const splash = document.getElementById('splash');
-const content = document.getElementById('main-content');
-
-// セッションストレージで動画再生情報を管理
-const hasPlayedIntro = sessionStorage.getItem('hasPlayedIntro');
-
-// 画像の読み込みを確認する関数
-function checkImagesLoaded() {
-    const images = document.querySelectorAll('img');
-    let loadedImagesCount = 0;
-
-    return new Promise((resolve) => {
-        images.forEach((img) => {
-            if (img.complete) {
-                loadedImagesCount++; // 画像が既にロード済みの場合
-            } else {
-                // 画像がロード完了したときのイベント
-                img.addEventListener('load', () => {
-                    loadedImagesCount++;
-                    if (loadedImagesCount === images.length) {
-                        resolve(); // すべての画像がロードされたらPromiseを解決
-                    }
-                });
-            }
-        });
-
-        console.log(loadedImagesCount)
-
-        // すべての画像が既にロードされている場合は即座にPromiseを解決
-        if (loadedImagesCount === images.length) {
-            resolve();
-        }
-    });
-}
-
 // スライダー
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -76,14 +39,17 @@ function swiping() {
 
 
 
-// 画像のロードが完了したら動画を再生
-checkImagesLoaded().then(() => {
-    if (!hasPlayedIntro) {
-        video.play(); // 画像がロードされた後に動画を再生
-    } else {
-        showContent(); // 動画をスキップし、直接コンテンツを表示
-    }
-});
+
+
+
+// 動画と要素の取得
+const video = document.getElementById('intro-video');
+const splash = document.getElementById('splash');
+const content = document.getElementById('main-content');
+
+// セッションストレージで動画再生情報を管理
+const hasPlayedIntro = sessionStorage.getItem('hasPlayedIntro');
+
 
 // 動画の終了イベントを監視して、スプラッシュを非表示にする
 video.onended = () => {
@@ -96,6 +62,10 @@ function showContent() {
     splash.style.display = 'none'; // スプラッシュスクリーンを非表示
     content.style.display = 'block'; // メインコンテンツを表示
 }
+
+
+
+
 
 
 
